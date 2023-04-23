d3a2fd93d4371827eaa2a34101f7c7e77b296d3d
dyves1 <ishimwedidieryves01@gmail.com>
2023-04-23 22:17:02 +0200

    remove message controller

--- src/Routes/allRoutes.js
+++ src/Routes/allRoutes.js
@@ -3,7 +3,7 @@ import blogRoute from "./blogRoute.js"
 import signupRoute from "./signupRoute.js"
 import loginRoute from "./loginRoute.js"
 import commentRoute from "./commentRoute.js"
-import messageRoute from "./messageRoute.js"
+// import messageRoute from "./messageRoute.js"
 
 
 const app =express()
@@ -14,7 +14,7 @@ router.use("/blogs",blogRoute)
 router.use("/signup",signupRoute)
 router.use("/login", loginRoute)
 router.use("/comments", commentRoute)
-router.use("/messages", messageRoute)
+// router.use("/messages", messageRoute)
 
 
 export default router
